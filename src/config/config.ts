66f4d7dee4293ea48e3dcf6507135cb1e61c85d0
Oleksandr Kuzmenko <eight.alex@gmail.com>
2024-05-13 14:55:52 +0300

    remove env

--- src/config/config.ts
+++ src/config/config.ts
@@ -6,11 +6,3 @@ export const API_URL: string = process.env.API_URL || '';
 export const BASE_URL: string = process.env.BASE_URL || '';
 export const CLIENT_URL: string = process.env.CLIENT_URL || '';
 export const APP_URL: string = process.env.APP_URL || '';
-
-// CLIENT_URL=http://localhost:5172
-// APP_URL=http://localhost:5173
-// API_URL=http://localhost:8080/api/v1
-//
-// #CLIENT_URL=https://pulsopus.dev
-// #APP_URL=https://app.pulsopus.dev
-// #API_URL=https://api.pulsopus.dev/api/v1
