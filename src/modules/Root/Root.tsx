b83b3c9b33de703db78a8f243e3dce657c2760b1
Oleksandr Kuzmenko <eight.alex@gmail.com>
2024-05-18 14:30:45 +0300

    fix start build env

--- src/modules/Root/Root.tsx
+++ src/modules/Root/Root.tsx
@@ -1,8 +1,11 @@
+import Stack from "@mui/material/Stack";
 import type { History } from "@remix-run/router";
 import { observer } from "mobx-react";
 import { memo } from 'react';
 import { unstable_HistoryRouter as HistoryRouter } from 'react-router-dom';
 
+import Typography from "@/components/Typography";
+import { APP_VERSION,IS_DEV } from "@/config";
 import { useStores } from "@/hooks";
 import { NotificationContainer } from '@/modules/Root/NotificationContainer';
 import { RootRoutes } from "@/modules/Root/RootRoutes.tsx";
@@ -11,23 +14,29 @@ import Theme from '@/theme';
 
 const BASELINE = '/';
 
+const BuildVersion = () => {
+    if(!IS_DEV) {
+        return;
+    }
+    return (
+        <Stack
+            sx={{
+                position: 'fixed',
+                bottom: 0,
+                left: 0,
+            }}
+        >
+            <Typography variant="caption1">v{APP_VERSION}</Typography>
+        </Stack>
+    );
+};
+
 const Root = observer(() => {
     const {
         rootStore: {
             routeStore: { history },
         },
     } = useStores();
-
-    // return (
-    //     <Provider store={store}>
-    //         <Theme>
-    //             <RootWrapper>
-    //                 <RouterProvider router={router}/>
-    //                 <NotificationContainer/>
-    //             </RootWrapper>
-    //         </Theme>
-    //     </Provider>
-    // );
 
     return (
         <HistoryRouter
@@ -38,6 +47,7 @@ const Root = observer(() => {
                 <RootWrapper>
                     <NotificationContainer/>
                     <RootRoutes/>
+                    <BuildVersion/>
                 </RootWrapper>
             </Theme>
         </HistoryRouter>
