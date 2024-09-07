8cddec6cba75ead4afd9e8ae950964fdce632a06
Oleksandr Kuzmenko <eight.alex@gmail.com>
2024-09-07 11:15:15 +0300

    minor fix

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.joq0r388d5o"
+    "revision": "0.rhnfo1uu398"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
